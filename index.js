c2e0c06256641a4a78e2841c70da6b3a29f368af
EliasFunes <eliasfunesriveros@gmail.com>
2018-03-14 17:43:15 -0300

    using passport, mongoose and bcrypt to compare and store user data

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config');
+
+// connect to the database and load models
+require('./server/models').connect(config.dbUri);
+
 
 const app = express();
 
@@ -10,14 +16,29 @@ app.use(express.static('./client/dist/'));
 //tell the app to parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended:false }));
 
+//pass the passport middleware
+app.use(passport.initialize());
+
+//load passport strategies
+const localSignupStrategy = require('./server/passport/local-signup');
+const localLoginStrategy = require('./server/passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+//pass the authentication checker middleware
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
 
 //routes
 const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 const port = 3000;
 app.listen(port, () => {
 	console.log(`Listening at http://localhost:${port}`)
 })
 
-//REFERENCES FROM => https://vladimirponomarev.com/blog/authentication-in-react-apps-creating-components
+//REFERENCES FROM => 1) https://vladimirponomarev.com/blog/authentication-in-react-apps-creating-components
+//					 2) https://vladimirponomarev.com/blog/authentication-in-react-apps-jwt
